a51dfbc7d8afbaa5ba7e991b9e5a445559b7a41a
michaelPMT <michael_patrick_96@hotmail.cm>
2016-02-09 08:43:15 -0500

    ac

---
+++ clase3/MateApp/src/pe/egcc/mateapp/service/MyMath.java
@@ -0,0 +1,26 @@
+package pe.egcc.mateapp.service;
+public class MyMath {
+
+    private MyMath() {
+    }
+    
+    public static long factorial(int n) {
+        return 0;
+    }
+    
+    public static int mcd(int n1 , int n2){
+        return 0;
+    }
+    
+    public static int mcm(int n1 , int n2){
+        return 0;
+    }
+    
+    public static String fibonacci(int n1){
+        return "";
+    }
+    
+    public static boolean primo(int n1){
+        return false;
+    }
+}
