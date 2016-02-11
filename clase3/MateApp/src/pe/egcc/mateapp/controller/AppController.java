2a6e9639e57348f42203dc242c6220f4d92a1e09
michaelPMT <michael_patrick_96@hotmail.com>
2016-02-11 06:24:47 -0500

    ac

--- clase3/MateApp/src/pe/egcc/mateapp/controller/AppController.java
+++ clase3/MateApp/src/pe/egcc/mateapp/controller/AppController.java
@@ -17,4 +17,19 @@ public class AppController {
        return MyMath.factorial(n);
     }
     
+    public int mcd(int n1, int n2) {
+        return MyMath.mcd(n1, n2);
+    }
+    
+    public int mcm(int n1, int n2){
+        return MyMath.mcm(n1, n2);
+    }
+    
+    public String fibonacci(int n){
+        return MyMath.fibonacci(n);
+    }
+    
+    public boolean primo(int n1){
+        return MyMath.primo(n1);
+    }
 }
--- clase3/MateApp/src/pe/egcc/mateapp/view/FibonacciView.java
+++ clase3/MateApp/src/pe/egcc/mateapp/view/FibonacciView.java
@@ -5,6 +5,8 @@
  */
 package pe.egcc.mateapp.view;
 
+import pe.egcc.mateapp.controller.AppController;
+
 /**
  *
  * @author Alumno
@@ -28,6 +30,10 @@ public class FibonacciView extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
+        tx1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        mostrar = new javax.swing.JTextArea();
 
         setClosable(true);
         setIconifiable(true);
@@ -36,29 +42,92 @@ public class FibonacciView extends javax.swing.JInternalFrame {
         setTitle("Fibonacci");
 
         jButton1.setText("Procesar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Ingresar Numero");
+
+        mostrar.setColumns(20);
+        mostrar.setRows(5);
+        jScrollPane1.setViewportView(mostrar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(101, 101, 101)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(125, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(99, 99, 99)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
+                                .addComponent(tx1, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(141, 141, 141)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(91, 91, 91)
+                .addGap(33, 33, 33)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(tx1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(29, 29, 29)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(114, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       /**
+        * //Dato
+        int n = Integer.parseInt(tx1.getText());
+        
+        //PRoceso
+        AppController control;
+        control = new AppController();
+        long f = control.factorial(n);
+        
+        //Reporte
+        String repo;
+        repo = "Factorial de " + n + " es " + f;
+        lblRepo.setText(repo);
+        */
+       
+       //Dato
+       int n = Integer.parseInt(tx1.getText());
+       
+       //Proceso
+        AppController control;
+        control = new AppController();
+        String fi = control.fibonacci(n);
+        
+        //Reporte
+        String repo;
+        repo = "Fibonaci de " + n + " es " + fi;
+        mostrar.setText(repo);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea mostrar;
+    private javax.swing.JTextField tx1;
     // End of variables declaration//GEN-END:variables
 }
